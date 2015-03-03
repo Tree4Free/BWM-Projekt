1fd8fa423caec9f22eb4cbcd1fdb22aae8864f4c
Roland <Roland@10.34.9.168>
2015-03-03 15:43:34 +0100

    Gui starten hinzugefügt

--- BWMProjekt/src/Start.java
+++ BWMProjekt/src/Start.java
@@ -1,7 +1,17 @@
+import javax.swing.SwingUtilities;
+
 
 public class Start {
 	
 	public static void main(String[] args) {
 		BackgroundWork bw=new BackgroundWork();
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				GuiFrame frame=new GuiFrame("BWM-Application");
+			}
+		});
 	}
 }
