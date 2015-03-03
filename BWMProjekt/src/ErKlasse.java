986660ccaa1277cf5fb9ffd79c9340733a6d8e93
Roland <Roland@10.34.9.168>
2015-03-03 15:05:56 +0100

    Ertragskonto Gefixt

--- BWMProjekt/src/ErKlasse.java
+++ BWMProjekt/src/ErKlasse.java
@@ -5,5 +5,17 @@ public class ErKlasse extends KKlassePassiv {
 		super(kk, 0);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void plusRechnen(float plus) {
+		// TODO Auto-generated method stub
+		super.minusRechnen(plus);
+	}
+
+	@Override
+	public void minusRechnen(float minus) {
+		// TODO Auto-generated method stub
+		super.plusRechnen(minus);
+	}
 	
 }
